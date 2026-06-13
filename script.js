64cc5b830fba8ea5cd27cdd41b7fa75f657bfdfa
Arpit Yadav <appuydv121@gmail.com>
2026-06-13 20:49:35 +0530

    Created animation not propely but did for 3 sections

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 Shery.mouseFollower()
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
+});
+
+Shery.makeMagnet(".magnet")
+
+Shery.hoverWithMediaCircle(".hvr" , {
+    images : [ "./image4.jpg" ,"./image1.jpg" , "./image3.jpg"]
+});
+gsap.registerPlugin(ScrollTrigger);
+gsap.to(".fleftelem" , {
+    scrollTrigger:{
+        trigger: "#f-images",
+        pin:true,
+        start:"top top",
+        end:"bottom bottom",
+        endTrigger:".last",
+          scrub:1
+    },
+    y:"-300%",
+    ease:Power1
+  
+})
+let sections = document.querySelectorAll(".fleftelem")
+Shery.imageEffect(".images", {
+  style: 1,
+  slideStyle: (setScroll) => {
+    sections.forEach((section , index)=>{
+        ScrollTrigger.create({
+            trigger:section,
+            start:"top top",
+            scrub:1,
+            onUpdate:function(prog){
+                setScroll(prog.progress+index)
+            }
+
+        })
+    })
+
+  },
 });
